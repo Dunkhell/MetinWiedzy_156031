ace7cd04e5649732c9c7eb30187c9bb8d0c00d5d
Dunkhell <ciemnoki12@gmail.com>
2022-06-08 00:02:29 +0200

    SVD almost perfectly working

--- lab10.py
+++ lab10.py
@@ -1,7 +1,8 @@
 import numpy as np
 from lab7 import qr_decomposition
-from lab9 import gauss
 import sympy as sp
+import math
+
 
 def matrix_k1(a):
     q = qr_decomposition(a)[0]
@@ -17,10 +18,41 @@ def matrix_eigenvalues(a):
     return new_a
 
 
+def gauss_reduction(matrix: np.ndarray):
+    column_len = matrix.shape[0]
+    result = []
+
+    for y in range(column_len - 1):
+        for x in range(column_len - 1):
+            if y + x >= column_len - 1:
+                continue
+            times = matrix[x + 1 + y][y] / matrix[y][y]
+            matrix[x + 1 + y] = matrix[x + 1 + y] - matrix[y] * times
+
+            done = True
+            for z in range(column_len - 1):
+                if matrix[column_len - 1][z] != 0:
+                    done = False
+
+            if done is True:
+                for k in range(column_len * 2 - 1, column_len - 1, -1):
+                    result.append(matrix[column_len - 1][k])
+                return result[::-1]
+
+        for x in range(1, column_len):
+            if y + x > column_len - 1:
+                continue
+            times = matrix[y][x + y] / matrix[x + y][x + y]
+            matrix[y] = matrix[y] - matrix[x + y] * times
+
+    return [matrix[i][column_len] / matrix[i][i] for i in range(column_len)]
+
+
 def calc_u(a):
-    u = np.array(a.shape)
+    u = []
     aat = np.dot(a, a.T)
-    lambdas = np.linalg.eigvals(aat)
+    lambdas = np.around(np.linalg.eigvals(aat), decimals=1)
+    lambdas[::-1].sort()
     for lambda_i in lambdas:
         aat_copy = np.copy(aat)
         for i in range(aat_copy.shape[0]):
@@ -28,20 +60,52 @@ def calc_u(a):
                 if i == j:
                     aat_copy[i][j] -= lambda_i
 
-        zeros = np.zeros((aat_copy.shape[0], 1))
-        aat_copy = np.concatenate((aat_copy, zeros), axis=1)
+        np_eye = np.eye(aat_copy.shape[0])
+        aat_copy = np.concatenate((aat_copy, np_eye), axis=1)
+        u_i = np.array(gauss_reduction(aat_copy))
+        u_i = u_i / np.sqrt(np.sum(u_i ** 2))
+        u.append(u_i.T)
+    return np.array(u), lambdas
 
-        u_i = gauss(aat_copy)
-        print(u_i)
+
+def calc_v(a):
+    v = []
+    ata = np.dot(a.T, a)
+    lambdas = np.around(np.linalg.eigvals(ata), decimals=1)
+    lambdas[::-1].sort()
+    for lambda_i in lambdas:
+        ata_copy = np.copy(ata)
+        for i in range(ata_copy.shape[0]):
+            for j in range(ata_copy.shape[1]):
+                if i == j:
+                    ata_copy[i][j] -= lambda_i
+
+        np_eye = np.eye(ata_copy.shape[0])
+        ata_copy = np.concatenate((ata_copy, np_eye), axis=1)
+        v_i = np.array(gauss_reduction(ata_copy))
+        v_i = v_i / np.sqrt(np.sum(v_i ** 2))
+        v.append(v_i.T)
+    return np.array(v)
 
 
 def svd(a: np.ndarray):
     m, n = a.shape
-    calc_u(a)
+    u, lambdas = calc_u(a)
+    print(f'\nU MATRIX:\n{u}')
+    v = calc_v(a)
+    print(f'\nV MATRIX:\n{v}')
+    singular_matrix = np.zeros((m, n))
+    for i in range(singular_matrix.shape[0]):
+        singular_matrix[i][i] = math.sqrt(lambdas[i])
+    print(f'\nSINGULAR MATRIX:\n{singular_matrix}\n')
+
+    print(np.around(np.dot(np.dot(u, singular_matrix), v), decimals=0))
 
 
 matrix_in = np.array([
     [1, 2, 0],
     [2, 0, 2]
 ])
+
 svd(matrix_in)
+# Wszystko działa tylko w złym miejscu znaki wstawia :(
--- lab9.py
+++ lab9.py
@@ -13,7 +13,24 @@ def gauss(matrix):
                     matrix[y][z] = matrix[y][z] - factor * matrix[x][z]
     return [matrix[i][column_len] / matrix[i][i] for i in range(column_len)]
 
-
+# def gauss(matrix):
+#     column_len = matrix.shape[0]
+#
+#     for y in range(column_len - 1):
+#         for x in range(column_len - 1):
+#             if y + x >= column_len - 1:
+#                 continue
+#             times = matrix[x + 1 + y][y] / matrix[y][y]
+#             matrix[x + 1 + y] = matrix[x + 1 + y] - matrix[y] * times
+#
+#         for x in range(1, column_len):
+#
+#             if y + x > column_len - 1:
+#                 continue
+#             times = matrix[y][x + y] / matrix[x + y][x + y]
+#             matrix[y] = matrix[y] - matrix[x + y] * times
+#
+#     return [matrix[i][column_len] / matrix[i][i] for i in range(column_len)]
 def macierz_wlasnosci(matrix, wartosc):
     for x in range(matrix.shape[0]):
         for y in range(matrix.shape[0]):
